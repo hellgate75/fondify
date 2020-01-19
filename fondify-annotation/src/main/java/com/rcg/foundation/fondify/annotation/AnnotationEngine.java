df2a179734b880ec12a79d006601190f45786896
hellgate75 <hellgate75@gmail.com>
2020-01-19 19:40:43 +0000

    First commit

---
+++ fondify-annotation/src/main/java/com/rcg/foundation/fondify/annotation/AnnotationEngine.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.rcg.foundation.fondify.annotation;
+
+/**
+ * @author Fabrizio Torelli (hellgate75@gmail.com)
+ *
+ */
+public final class AnnotationEngine {
+
+	/**
+	 * Private blocked constructor
+	 */
+	private AnnotationEngine() {
+		throw new IllegalArgumentException("AnnotationEngine: Unable to make instance of Engine Class");
+	}
+
+	/**
+	 * Run annotation engine
+	 * @param instance Main Class
+	 */
+	public static void run(Class<?> instance) {
+		
+	}
+}
